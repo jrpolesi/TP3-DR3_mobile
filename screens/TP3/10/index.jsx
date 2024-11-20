51682da59aa6f540158944b31fe891653b9865d2
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-11-20 16:34:16 -0300

    ex11

--- screens/TP3/10/index.jsx
+++ screens/TP3/10/index.jsx
@@ -1,26 +1,34 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import { ProductsList } from "./components/ProductsList";
 
 const baseUrl = "https://dfef-dmrn-tps-default-rtdb.firebaseio.com";
 
 export default function App() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch(`${baseUrl}/products.json`);
-      const data = await res.json();
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${baseUrl}/products.json`);
+        const data = await res.json();
 
-      const resolvedData = Object.entries(data).map(([id, value]) => ({
-        id,
-        description: value.descricao,
-        name: value.nome,
-        price: value.preco,
-        image: value.imagens?.length ? value.imagens[0] : undefined,
-      }));
+        const resolvedData = Object.entries(data).map(([id, value]) => ({
+          id,
+          description: value.descricao,
+          name: value.nome,
+          price: value.preco,
+          image: value.imagens?.length ? value.imagens[0] : undefined,
+        }));
 
-      setData(resolvedData);
+        setData(resolvedData);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     fetchData();
@@ -28,7 +36,13 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <ProductsList products={data} />
+      {isLoading ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" />
+        </View>
+      ) : (
+        <ProductsList products={data} />
+      )}
     </View>
   );
 }
@@ -36,5 +50,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
